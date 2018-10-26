8474ab1c83beca8650914d0be2d606104dd993fb
Daniel Maldonado <daniel.maldonado@mercadolibre.com>
2018-10-26 13:53:23 -0300

    remove utils

--- src/URLParser/QueryParams.js
+++ src/URLParser/QueryParams.js
@@ -1,15 +1,23 @@
-const asHash = require('./utils/asHash')
-
 const extractQuery = url => {
     const queryStart = url.indexOf('?')
     return queryStart === -1 ? "" : url.slice(queryStart + 1)
+}
+const splitKeyValueArray = keyValue => keyValue.split('=')
+const reduceToSingleObject = (a, b) => Object.assign({}, a, b)
+const buildKeyValueHash = ([key, value]) => {
+    return {
+        [key]: Number.isNaN(parseInt(value)) ? value : parseInt(value)
+    }
 }
 
 const QueryString = url => {
     const queryString = extractQuery(url)
     return {
         asHash() {
-            return asHash(queryString.split("&").map(keyValue => keyValue.split('=')))
+            return queryString.split("&")
+            .map(splitKeyValueArray)
+            .map(buildKeyValueHash)
+            .reduce(reduceToSingleObject)
         }
     }
 }
--- src/URLParser/utils/asHash.js
+++
@@ -1,9 +0,0 @@
-const asHash = keyValueArr => {
-    return keyValueArr.map(([key, value]) => {
-        return {
-            [key]: Number.isNaN(parseInt(value)) ? value : parseInt(value)
-        }
-    })
-    .reduce((a, b) => Object.assign({}, a, b))
-}
-module.exports = asHash
